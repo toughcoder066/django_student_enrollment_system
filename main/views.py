ec8c2b88bcbd3188f2cf3ba35ff859801a548306
toughcoder066 <toughcoder066@gmail.com>
2023-08-03 18:07:01 +0000

    generic views

--- main/views.py
+++ main/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.contrib.auth.models import User
+from .serializers import StudentSerializer
+from .models import Student
+from rest_framework import generics
+from rest_framework.permissions import IsAdminUser,IsAuthenticated
 
 # Create your views here.
+
+class StudentList(generics.ListCreateAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+    permission_classes = [IsAuthenticated]
+
+class StudentDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+    permission_classes = [IsAuthenticated]
+
